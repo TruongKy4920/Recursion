356375495d84f6f865f99ed3ca46e1f38281c61c
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-07-29 13:34:22 +0700

    new search folder

---
+++ recursion/format.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <math.h>
+#define MAX 100
+using namespace std;
+
+std::ifstream input("input.txt");
+std::ofstream output("output.txt");
+
+
+
+
+void solution(){
+   
+}
+
+int main(){
+    std::string line;
+      input>>n>>m;
+      
+      solution();
+    // solution();
+}
